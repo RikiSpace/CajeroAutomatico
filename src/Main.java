b63871973244f2371a3a08e6aeea27daf712d3cb
ESTUDIANTE <lopez.rikelmer@iemodeliaibague.edu.co>
2023-10-03 16:10:44 -0500

    Correcion agregué ciclo While

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,28 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        final int saldo_inicial = 100000;
+        int saldo = 100000;
         int opcion = 0;
-        float dineroingresado, saldocuenta, retirodinero;
+        float dineroingresado, retirodinero;
+        while (opcion != 3) {
         opcion = Integer.parseInt(JOptionPane.showInputDialog("bienvenido al cajero automatico coco loco\n"
                 + "1.desea ingrsar dinero a su cuenta\n"
                 + "2.desea sacar dinero de su cuenta\n"
                 + "3.salir"));
-        while (opcion != 3) {
 
             switch (opcion) {
                 case 1:
                     dineroingresado = Float.parseFloat(JOptionPane.showInputDialog("dijite la cantidad de dinero que va a ingresar a su cuenta"));
-                    saldocuenta = saldo_inicial + dineroingresado;
-                    JOptionPane.showMessageDialog(null, "el dinero que tienes en tu cuenta es de" + saldocuenta);
+                    saldo = (int) (saldo + dineroingresado);
+                    JOptionPane.showMessageDialog(null, "el dinero que tienes en tu cuenta es de" + saldo);
                     break;
                 case 2:
                     retirodinero = Float.parseFloat(JOptionPane.showInputDialog("dijite la cantidad de dinero que desea retirar de la cuenta"));
-                    if (retirodinero > saldo_inicial) {
+                    if (retirodinero > saldo) {
                         JOptionPane.showMessageDialog(null, "el dinero que desea ingresar no esta disponible trabaje mas");
                     } else {
-                        saldocuenta = saldo_inicial - retirodinero;
-                        JOptionPane.showMessageDialog(null, "el saldo de la cuenta esta en" + saldocuenta);
+                        saldo = (int) (saldo - retirodinero);
+                        JOptionPane.showMessageDialog(null, "el saldo de la cuenta esta en" + saldo);
 
                     }
                     break;
